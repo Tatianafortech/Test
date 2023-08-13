5aacee581e6bb5262a5fe9336743723e3da44e98
Tatianafortech <111741943+Tatianafortech@users.noreply.github.com>
2023-08-13 17:24:19 +0300

    Update test1.groovy

--- test1.groovy
+++ test1.groovy
@@ -1,1 +1,32 @@
+pipelineJob("Repo QA job"){
+  parameters {
+    stringParam('test', '', 'DEPLOYMENT to QA')
+  }
+  properties {
+    githubProjectUrl("https://github.com/Tatianafortech/Test")
+  }
+  definition {
+    cps {
+      sandbox(true)
+      script('''
+def NEW_IMAGE
 
+pipeline {
+  agent any
+  stages {
+    stage('Run tests') {
+      steps {
+        sh """echo test """
+      }
+    }
+  }
+  post {
+    always {
+      cleanWs()
+    }
+  }
+}
+      '''.stripIndent())
+     }
+   }
+}
